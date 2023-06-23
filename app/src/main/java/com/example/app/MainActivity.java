9580d44ed43d7a80104db65fc9ce1b083018ca01
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-23 18:35:49 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends Activity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
 
+                injectJavaScript();
+                
                 // Inject JavaScript code into the main page
                 String mainPageJsCode = "var videos = document.getElementsByTagName('video');" +
                         "for (var i = 0; i < videos.length; i++) {" +
@@ -93,6 +95,14 @@ public class MainActivity extends Activity {
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+        private void injectJavaScript() {
+        String javascriptCode = "(function() {\n" +
+                "// Add a button to the page\r\nvar revealButton = document.createElement(\"button\");\r\nrevealButton.innerHTML = \"Show Page Source\";\r\nrevealButton.style.position = \"fixed\";\r\nrevealButton.style.top = \"20px\";\r\nrevealButton.style.left = \"20px\";\r\nrevealButton.style.padding = \"10px\";\r\nrevealButton.style.backgroundColor = \"#3498db\";\r\nrevealButton.style.color = \"#fff\";\r\nrevealButton.style.border = \"none\";\r\nrevealButton.style.borderRadius = \"5px\";\r\ndocument.body.appendChild(revealButton);\r\n\r\n// Add an event listener to the button\r\nrevealButton.addEventListener(\"click\", function() {\r\n  // Create a new window or iframe to display the source code\r\n  var sourceWindow = window.open(\"\", \"_blank\");\r\n  sourceWindow.document.write(\"<pre><code>\" + escapeHtml(document.documentElement.outerHTML) + \"</code></pre>\");\r\n\r\n  // Add a close button\r\n  var closeButton = sourceWindow.document.createElement(\"button\");\r\n  closeButton.innerHTML = \"Close\";\r\n  closeButton.style.padding = \"10px\";\r\n  closeButton.style.backgroundColor = \"#e74c3c\";\r\n  closeButton.style.color = \"#fff\";\r\n  closeButton.style.border = \"none\";\r\n  closeButton.style.borderRadius = \"5px\";\r\n  sourceWindow.document.body.appendChild(closeButton);\r\n\r\n  // Add an event listener to the close button\r\n  closeButton.addEventListener(\"click\", function() {\r\n    sourceWindow.close();\r\n  });\r\n\r\n  // Add a search input box to the source code container\r\n  var searchInput = sourceWindow.document.createElement(\"input\");\r\n  searchInput.type = \"text\";\r\n  searchInput.placeholder = \"Search\";\r\n  searchInput.style.padding = \"10px\";\r\n  searchInput.style.marginTop = \"20px\";\r\n  searchInput.style.border = \"1px solid #ccc\";\r\n  sourceWindow.document.body.appendChild(searchInput);\r\n\r\n  // Add a container for match count information\r\n  var matchCount = sourceWindow.document.createElement(\"div\");\r\n  matchCount.id = \"match-count\";\r\n  matchCount.style.marginTop = \"10px\";\r\n  sourceWindow.document.body.appendChild(matchCount);\r\n\r\n  // CSS styles for highlighting search term\r\n  var styles = sourceWindow.document.createElement(\"style\");\r\n  styles.innerHTML = \".highlight { background-color: yellow; }\";\r\n  sourceWindow.document.head.appendChild(styles);\r\n});\r\n\r\n// Function to escape HTML special characters\r\nfunction escapeHtml(unsafe) {\r\n  return unsafe.replace(/</g, \"&lt;\").replace(/>/g, \"&gt;\");\r\n}\r\n" +
+                "})();";
+
+        webView.evaluateJavascript(javascriptCode, null);
     }
 
     @Override
